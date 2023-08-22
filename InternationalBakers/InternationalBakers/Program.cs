044ca8ed248679b258b188b27984c4eb0d1efc04
siddharth-dhawan <siddharthdwn@gmail.com>
2023-08-22 22:10:43 +0000

    AAD Integration

--- InternationalBakers/InternationalBakers/Program.cs
+++ InternationalBakers/InternationalBakers/Program.cs
@@ -1,13 +1,35 @@
 using InternationalBakers.Data;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
+builder.Services.AddRazorPages()
+     .AddMicrosoftIdentityUI();
 
 var connectionString = builder.Configuration.GetConnectionString("ibdb");
 builder.Services.AddDbContext <Sbazuresqldb221936360Context> (x => x.UseSqlServer(connectionString));
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.Secure = CookieSecurePolicy.Always;
+});
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddStackExchangeRedisCache(option =>
 {
@@ -27,6 +49,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCookiePolicy();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
